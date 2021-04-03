f5f17c855ed135a6257a7135561a8c847633fbda
Sanskar-Kushwah <sanskar0734@gmail.com>
2021-04-03 15:04:33 +0530

    interntask12

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const pass2 = require('passport-google-oauth20');
+const app = express();
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+
+require('./setup');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.set('view engine','ejs');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cookieSession({
+  name: 'intern-session',
+  keys: ['key1', 'key2']
+}))
+
+const isLoggedIn = (req, res, next) => {
+  if (req.user) {
+      next();
+  } else {
+      res.sendStatus(401);
+  }
+}
+
+app.get('/', (req,res)=>{
+  res.render('home');
+})
+
+app.get('/success',(req,res)=>{
+  res.render('show');
+})
+app.get('/login',(req,res)=>{
+  res.send("Something went wrong");
+})
+
+app.get('/google',passport.authenticate('google', { scope: ['profile','email'] }));
+
+app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/success');
+  });
+
+
+  app.get('/logout', (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect('/');
+})
+
+app.listen(3000,()=>{
+  console.log("This is running");
+})
---
+++ setup.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const mongoose = require('mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('./user');
+
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connection Open");
+});
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+
+  
+passport.use(new GoogleStrategy({
+    clientID: "865219137619-q73sn6ohpuebftlakie3u15h34v3mssv.apps.googleusercontent.com",
+    clientSecret: "-uocLHpouUeeP4TKTO0MCuB-",
+    callbackURL: "http://localhost:3000/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOne({googleId: profile.id}).then(existingUser => {
+      if (existingUser) {
+          done(null, existingUser);
+      } else {
+          new User({googleId: profile.id}).save().then(user => done(null, user));
+      }
+  });
+  }
+));
